93d77cc9fc032b8026cbbb253f8db898c2c7f556
eeyjl11 <92301581+eeyjl11@users.noreply.github.com>
2022-07-20 02:08:52 +0100

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -1,8 +1,13 @@
 # Will create database for each stock that holds the calculated fundamentals fom set date to current
 
 class Dataset:
-    def checkDatabaseExistence(self):
-        print("Checking if database exists")
-
     def populateDatabase(self):
+        # Needs to check if csv file exists
+        # If not, create
+        # Populate array from set start date to current time
+        
+        # If file does exist check if the previous time is current time minus interval
+        # If so then calculate current time and add to csv
+        # If not populate from last date to current time
+        
         print("Will populate database from set date to current time")
